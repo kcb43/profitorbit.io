48364543ef7c290d33f93f5bbe9c2d343e7e999f
casey <casey@example.com>
2025-12-22 03:18:41 -0500

    Add page-context console logs for bridge API

--- extension/profit-orbit-bridge.js
+++ extension/profit-orbit-bridge.js
@@ -35,12 +35,15 @@ console.log('Profit Orbit Extension: Document ready state:', document.readyState
       // Expose API that will communicate with content script via postMessage
       window.ProfitOrbitExtension = {
         queryStatus: function() {
+          console.log('[PAGE] Profit Orbit Extension: queryStatus() called');
           window.postMessage({ type: 'PROFIT_ORBIT_QUERY_STATUS' }, '*');
         },
         isAvailable: function() {
+          console.log('[PAGE] Profit Orbit Extension: isAvailable() called - returning true');
           return true; // Assume available if script is injected
         },
         getAllStatus: function(callback) {
+          console.log('[PAGE] Profit Orbit Extension: getAllStatus() called');
           const requestId = Math.random().toString(36).substring(7);
           window.__ProfitOrbitCallbacks = window.__ProfitOrbitCallbacks || {};
           window.__ProfitOrbitCallbacks[requestId] = callback;
@@ -51,7 +54,8 @@ console.log('Profit Orbit Extension: Document ready state:', document.readyState
         }
       };
       
-      console.log('Profit Orbit Extension: Bridge API injected into page context', window.ProfitOrbitExtension);
+      console.log('[PAGE] Profit Orbit Extension: Bridge API injected into page context', window.ProfitOrbitExtension);
+      console.log('[PAGE] Profit Orbit Extension: API methods:', Object.keys(window.ProfitOrbitExtension));
       
       // Dispatch a custom event to notify React app that bridge is ready
       window.dispatchEvent(new CustomEvent('profitOrbitBridgeReady', {
