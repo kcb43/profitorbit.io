0071849519e9d65a9bfc78ee134777cbc351da6b
casey <casey@example.com>
2026-01-25 15:46:50 -0500

    Remove debug setFacebookDtsg function - automatic capture working

--- extension/background.js
+++ extension/background.js
@@ -12,16 +12,6 @@ console.log('EXT BUILD:', EXT_BUILD);
 // Load Facebook API module for GraphQL calls
 importScripts('facebook-api.js');
 
-// Debug function to manually set fb_dtsg token
-self.setFacebookDtsg = function(token) {
-  chrome.storage.local.set({
-    'facebook_dtsg': token,
-    'facebook_dtsg_timestamp': Date.now(),
-  }).then(() => {
-    console.log('✅ Manually set fb_dtsg token:', token.substring(0, 30) + '...');
-  });
-};
-
 // Auto-capture fb_dtsg from any open Facebook tabs on extension load
 async function autoCaptureFacebookToken() {
   try {
